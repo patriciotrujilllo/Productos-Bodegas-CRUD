4f486589fef2efd84da75b147e6e2bc24a77a407
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-09 09:59:23 -0300

    add enviroment variables

--- modulos/conexion.php
+++ modulos/conexion.php
@@ -1,15 +1,24 @@
 <?php
 // Conexion a base de datos por medio de PDO
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-$dsn = 'mysql:host=localhost;dbname=crud_bodegas_productos;charset=utf8mb4';
-$username = 'root';
-$password = '';
+$host = $_ENV['host'];
+$dbname = $_ENV['dbname'];
+$username = $_ENV['username'];
+$password = $_ENV['password'];
+
+
+$dsn = "mysql:host=$host;
+dbname=$dbname;
+charset=utf8mb4";
+$username = $username;
+$password = $password;
 
 try {
     $conexion = new PDO($dsn, $username, $password);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Crea una excepcion que sera mostrado en el catch
-} catch(PDOException $e) {
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Crea una excepcion que sera mostrado en el catch
+} catch (PDOException $e) {
     die("Error al conectar a la base de datos: " . $e->getMessage());
 }
-
-?>
