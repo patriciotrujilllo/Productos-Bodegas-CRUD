7e67a31b58552f3171b90a704fe44a456d85fb7c
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2024-02-29 18:23:41 -0300

    remove delete logic for the modal

--- script/productos.js
+++ script/productos.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     let editaModal = document.querySelector('#Modal_Productos_editar')
-    let eliminaModal = document.querySelector('#Modal_Productos_eliminar')
+    // let eliminaModal = document.querySelector('#Modal_Productos_eliminar')
 
     editaModal.addEventListener('shown.bs.modal', event => {
         let button = event.relatedTarget
@@ -42,27 +42,26 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 
-    eliminaModal.addEventListener('shown.bs.modal', event => {
-        let button = event.relatedTarget
-        let id = button.getAttribute('data-bs-id')
-        eliminaModal.querySelector('.modal-footer #id_delete').value = id
-    })
-    //Se ejecuara esta funcion cuando se haga click en el elemento con id btnEjecutar
-    //dentro de esta funcion se obtendra el id que fue enviado al modal 
-    //Se procede a llamar a la funcion para eliminar los datos
-    document.getElementById("btnEjecutar").addEventListener("click", function () {
-        let variable = document.querySelector('.modal-footer #id_delete');
-        let valor = variable.value;
-        // console.log(valor);
-        eliminar(valor);
-    });
+    // eliminaModal.addEventListener('shown.bs.modal', event => {
+    //     let button = event.relatedTarget
+    //     let id = button.getAttribute('data-bs-id')
+    //     eliminaModal.querySelector('.modal-footer #id_delete').value = id
+    // })
+    // //Se ejecuara esta funcion cuando se haga click en el elemento con id btnEjecutar
+    // //dentro de esta funcion se obtendra el id que fue enviado al modal 
+    // //Se procede a llamar a la funcion para eliminar los datos
+    // document.getElementById("btnEjecutar").addEventListener("click", function () {
+    //     let variable = document.querySelector('.modal-footer #id_delete');
+    //     let valor = variable.value;
+    //     // console.log(valor);
+    //     eliminar(valor);
+    // });
 
     document.getElementById("btnGuardar").addEventListener("click", function () {
         let newProducto = {
             nombre: document.querySelector('.modal-body #nombre_producto').value,
             descripcion: document.querySelector('.modal-body #descripcion_producto').value
         };
-        console.log(newProducto);
         guardar(newProducto);
     });
 
