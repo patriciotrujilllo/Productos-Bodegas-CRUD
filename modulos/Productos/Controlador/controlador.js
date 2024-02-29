ca3181117856f9294fc8d40fc25172f110b820fe
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2024-02-29 18:28:25 -0300

    change the static data listing and row creation to dynamic one

--- modulos/Productos/Controlador/controlador.js
+++ modulos/Productos/Controlador/controlador.js
@@ -1,23 +1,122 @@
 var productos = [];
 const url = 'modulos/Productos/Api/Api-Rest.php';
 
-const llenarTabla = () => {
-    document.querySelector('#tablaProductos tbody').innerHTML = '';
-    productos.forEach(producto => {
-        document.querySelector('#tablaProductos tbody').innerHTML +=
-            `<tr>
-            <td class='align-middle'>${producto['Nombre_producto']}</td>
-            <td class='align-middle'>${producto['Descripcion']}</td>
-            <td> 
-            <div class='text-center'>
-            <div class='btn-group'>
-                <button data-bs-toggle='modal' data-bs-target='#Modal_Productos_editar' class='btn btn-primary btnEditar' data-bs-id='${producto['Id_producto']}'>Editar</button>
-                <button data-bs-toggle='modal' data-bs-target='#Modal_Productos_eliminar' class='btn btn-danger btnEditar' data-bs-id='${producto['Id_producto']}'>Eliminar</button>
-            </div>
-            </div>
+const tabla = document.querySelector('#tablaProductos tbody')
+const createNodo = ({ Nombre_producto, Descripcion, Id_producto }) => {
+    // const containerTable = document.querySelector('#tablaProductos tbody')
 
-            </td>
-        </tr>`
+    //Se crea la fila
+    const row = document.createElement('tr')
+    row.dataset.productId = Id_producto
+
+    //Se crea la primera columna, nombre del producto
+
+    const nombreProducto = document.createElement('td')
+    nombreProducto.className = 'align-middle'
+    nombreProducto.textContent = Nombre_producto
+
+    //Se crea columna, descriptcio del producto
+
+    const descripcionProducto = document.createElement('td')
+    descripcionProducto.className = 'align-middle'
+    descripcionProducto.textContent = Descripcion
+
+    //Columna de las acciones
+
+    const accionesCelda = document.createElement('td')
+    const accionesDiv = document.createElement('td')
+    accionesDiv.className = 'text-center'
+
+    //Div contenedor de acciones
+
+    const btnGroupDiv = document.createElement('div')
+    btnGroupDiv.className = 'btn-group'
+
+    //Boton de editar
+
+    const editarButton = document.createElement('button')
+    editarButton.className = 'btn btn-primary btnEditar'
+    editarButton.dataset.bsToggle = 'modal'
+    editarButton.dataset.bsTarget = '#Modal_Productos_editar'
+    editarButton.dataset.bsId = Id_producto
+    editarButton.textContent = 'Editar'
+
+    //Boton eliminar
+
+    const eliminarButton = document.createElement('button')
+    eliminarButton.className = 'btn btn-danger btnEditar'
+    eliminarButton.dataset.bsToggle = 'modal'
+    eliminarButton.dataset.bsTarget = '#Modal_Productos_eliminar'
+    eliminarButton.dataset.bsId = Id_producto
+    eliminarButton.textContent = 'Eliminar'
+
+    //eliminar fila
+    eliminarButton.addEventListener('click', function () {
+        const idProducto = this.dataset.bsId;
+        // Configura el modal de eliminación para confirmar antes de eliminar
+        document.getElementById('btnEjecutar').addEventListener('click', function () {
+            // const row = document.querySelector(`tr[data-product-id="${idProducto}"]`)
+            const row = document.querySelector(`#tablaProductos tbody tr[data-product-id="${idProducto}"]`);
+
+            eliminar({ id: idProducto, row })
+            // Desvincula el evento para evitar múltiples llamadas si se presiona el botón de eliminar varias veces
+            // document.querySelector('btnEjecutar').removeEventListener('click', arguments.callee)
+        }, { once: true })
+    })
+
+    //Se agregan los hijos a los elementos padres
+
+    //se agregan los botones al div contenedor
+
+    btnGroupDiv.appendChild(editarButton)
+    btnGroupDiv.appendChild(eliminarButton)
+
+    //Se agrega el div dentro del otro div y dentro de la celda td
+
+    accionesDiv.appendChild(btnGroupDiv)
+    accionesCelda.appendChild(accionesDiv)
+
+    // A la fila td,que es el padre de los elementos, se le agregan sus 3 elemntos hijos
+
+    row.appendChild(nombreProducto)
+    row.appendChild(descripcionProducto)
+    row.appendChild(accionesCelda)
+
+    //Se agrega la fila a la tabla
+
+    tabla.appendChild(row)
+
+
+}
+
+const actualizarNodo = ({ Id_producto, Nombre_producto, Descripcion }) => {
+    const fila = document.querySelector(`#tablaProductos tbody tr[data-product-id="${Id_producto}"]`)
+
+    const nombreColumna = fila.querySelector('td:nth-child(1)')
+    const descripcionColumna = fila.querySelector('td:nth-child(2)')
+
+    nombreColumna.textContent = Nombre_producto
+    descripcionColumna.textContent = Descripcion
+}
+
+const llenarTabla = () => {
+    // document.querySelector('#tablaProductos tbody').innerHTML = '';
+    productos.forEach(producto => {
+        createNodo(producto)
+        // document.querySelector('#tablaProductos tbody').innerHTML +=
+        //     `<tr>
+        //     <td class='align-middle'>${Nombre_producto}</td>
+        //     <td class='align-middle'>${Descripcion}</td>
+        //     <td> 
+        //     <div class='text-center'>
+        //     <div class='btn-group'>
+        //         <button data-bs-toggle='modal' data-bs-target='#Modal_Productos_editar' class='btn btn-primary btnEditar' data-bs-id='${Id_producto}'>Editar</button>
+        //         <button data-bs-toggle='modal' data-bs-target='#Modal_Productos_eliminar' class='btn btn-danger btnEditar' data-bs-id='${Id_producto}'>Eliminar</button>
+        //     </div>
+        //     </div>
+
+        //     </td>
+        // </tr>`
     });
 }
 
@@ -48,15 +147,16 @@ const GetId = (id) => {
 
 GetAll();
 
-const eliminar = (id) => {
+const eliminar = ({ id, row }) => {
     axios({
         method: 'DELETE',
         url: url + `?Id_producto=${id}`,
         responseType: 'json'
     }).then(res => {
-        GetAll();
+        // Elimina la fila de la tabla después de eliminar el producto en la base de datos
+        tabla.removeChild(row)
     }).catch(error => {
-        console.error(error);
+        console.error(error)
     })
 }
 
@@ -67,7 +167,7 @@ const actualizar = (producto) => {
         responseType: 'json',
         data: producto
     }).then(res => {
-        GetAll();
+        actualizarNodo({ Id_producto: producto['id'], Nombre_producto: producto.nombre, Descripcion: producto.descripcion })
     }).catch(error => {
         console.error(error);
     })
@@ -80,7 +180,7 @@ const guardar = (producto) => {
         responseType: 'json',
         data: producto
     }).then(res => {
-        GetAll();
+        createNodo({ Nombre_producto: producto.nombre, Descripcion: producto.descripcion, Id_producto: res.data.id_producto })
     }).catch(error => {
         console.error(error);
     })
