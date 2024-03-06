67ed51676712e18d7112fcbf9bdb13f1db639ded
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2024-03-06 10:42:12 -0300

    Create list of Bodegas

--- modulos/Bodegas/Controlador/controlador.js
+++ modulos/Bodegas/Controlador/controlador.js
@@ -1,23 +1,77 @@
-var bodegas = [];
+let bodegas = [];
 const url = 'modulos/Bodegas/Api/Api-Rest.php';
+const tabla = document.querySelector('#tablaBodegas tbody')
 
-const llenarTabla = () => {
-    document.querySelector('#tablaBodegas tbody').innerHTML = '';
+const llenarTabla = (bodegas) => {
     bodegas.forEach(bodega => {
-        document.querySelector('#tablaBodegas tbody').innerHTML +=
-            `<tr>
-            <td class='align-middle'>${bodega['Nombre']}</td>
-            <td> 
-            <div class='text-center'>
-            <div class='btn-group'>
-                <button data-bs-toggle='modal' data-bs-target='#Modal_Bodegas_editar' class='btn btn-primary btnEditar' data-bs-id='${bodega['Id_bodega']}'>Editar</button>
-                <button data-bs-toggle='modal' data-bs-target='#Modal_Bodegas_eliminar' class='btn btn-danger btnEditar' data-bs-id='${bodega['Id_bodega']}'>Eliminar</button>
-            </div>
-            </div>
-
-            </td>
-        </tr>`
+        createNodo({ nombreBodega: bodega.Nombre, idBodega: bodega.Id_bodega })
     });
+}
+
+const createNodo = ({ nombreBodega, idBodega }) => {
+
+    //Se crea la fila
+    const row = document.createElement('tr')
+    row.dataset.bodegaId = idBodega
+
+    //Se crea la primera columna, nombre del producto
+
+    const n_Bodega = document.createElement('td')
+    n_Bodega.className = 'align-middle'
+    n_Bodega.textContent = nombreBodega
+
+    //Columna de las acciones
+
+    const celdaAcciones = document.createElement('td')
+    const divbtn = document.createElement('div')
+    divbtn.className = 'text-center'
+
+    //Div contenedor de acciones
+
+    const divAcciones = document.createElement('div')
+    divAcciones.className = 'btn-group'
+
+    //Boton de editar
+
+    const editButton = document.createElement('button')
+    editButton.dataset.bsToggle = 'modal'
+    editButton.dataset.bsTarget = '#Modal_Bodegas_editar'
+    editButton.className = 'btn btn-primary btnEditar'
+    editButton.dataset.bsId = idBodega
+    editButton.textContent = 'Editar'
+
+    //Boton eliminar
+
+    const deleteButton = document.createElement('button')
+    deleteButton.dataset.bsToggle = 'modal'
+    deleteButton.dataset.bsTarget = '#Modal_Bodegas_eliminar'
+    deleteButton.className = 'btn btn-danger btnEditar'
+    deleteButton.dataset.bsId = idBodega
+    deleteButton.textContent = 'Eliminar'
+
+
+    //eliminar fila
+
+
+    //se agregan los botones al div contenedor
+
+    divAcciones.appendChild(editButton)
+    divAcciones.appendChild(deleteButton)
+
+    //Se agrega el div dentro del otro div y dentro de la celda td
+
+    divbtn.appendChild(divAcciones)
+    celdaAcciones.appendChild(divbtn)
+
+    // A la fila td,que es el padre de los elementos, se le agregan sus 3 elemntos hijos
+
+    row.appendChild(n_Bodega)
+    row.appendChild(celdaAcciones)
+
+    //Se agrega la fila a la tabla
+
+    tabla.appendChild(row)
+
 }
 
 const GetAll = () => {
@@ -26,8 +80,9 @@ const GetAll = () => {
         url: url,
         responseType: 'json'
     }).then(res => {
-        this.bodegas = res.data;
-        llenarTabla();
+        bodegas = res.data
+        console.log(bodegas)
+        llenarTabla(bodegas)
     }).catch(error => {
         console.error(error);
     })
@@ -39,7 +94,7 @@ const GetId = (id) => {
         url: url + `?Id_bodega=${id}`,
         responseType: 'json'
     }).then(res => {
-        this.bodegas = res.data;
+        bodegas = res.data;
     }).catch(error => {
         console.error(error);
     })
